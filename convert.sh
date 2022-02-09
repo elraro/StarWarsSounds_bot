bf2cdf90faf60d18e90b1cb7d75f0b8753c50cb6
elraro <elraro@elraro.eu>
2022-02-09 23:22:18 +0100

    Dockerized convert script and The Book of Boba Fett

--- convert.sh
+++ convert.sh
@@ -1,3 +1,4 @@
-for f in StarWarsSounds/*; do
-	ffmpeg -i "$f" -map_metadata -1 -c:a libopus -b:a 64k -vbr on -compression_level 10 converted/${f##*/}
+for f in raw/*; do
+	name=/${f##*/}
+	docker run --rm -it -v $(pwd)/raw/:/raw -v $(pwd)/StarWarsSounds/:/StarWarsSounds linuxserver/ffmpeg -i /"$f" -map_metadata -1 -c:a libopus -b:a 64k -vbr on -compression_level 10 /StarWarsSounds/${name/%mp3/ogg}
 done
